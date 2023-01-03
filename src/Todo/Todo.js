2719329af485d964e94bf0711850c5cdc457a195
Agailh <agailham86@gmail.com>
2023-01-03 15:05:27 +0700

    add feature

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,16 +1,18 @@
 import "./Todo.css";
 import TodoList from "./todo-list/TodoList";
+import todoCreate from "./todo-create/todoCreate";
 
 const Todo = () => {
-  const todos = () => [
+  const todos = [
     { id: 1, title: "Eat" },
-    { id: 2, title: "code" },
+    { id: 2, title: "sleb" },
     { id: 3, title: "uweee" },
   ];
 
   return (
     <div>
       <h3>todo list</h3>
+      <todoCreate />
       <TodoList dataTodos={todos} />
     </div>
   );
---
+++ src/Todo/todo-create/todoCreate.js
@@ -0,0 +1,12 @@
+import "./todoCreate.css";
+
+const todoCreate = () => {
+  return (
+    <form className="todo-form">
+      <input type="text" />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
+
+export default todoCreate;
