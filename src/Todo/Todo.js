effcf377e75a9027a2147151bfa4527530140c18
Agailh <agailham86@gmail.com>
2023-01-03 13:45:14 +0700

    s

---
+++ src/Todo/Todo.js
@@ -0,0 +1,13 @@
+import "./Todo.css";
+import TodoList from "./todo-list/TodoList";
+
+const Todo = () => {
+  return (
+    <div>
+      <h3>todo list</h3>
+      <TodoList />
+    </div>
+  );
+};
+
+export default Todo;
---
+++ src/Todo/todo-list/TodoList.js
@@ -0,0 +1,12 @@
+import "./TodoList.css";
+
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Leran React </li>
+      <li> Stream </li>
+      <li> Stream </li>
+    </ul>
+  );
+};
+export default TodoList;
